dbde2633b3f62a167c99e4ee83eea5315bd78bfc
lgarg228 <lxg1299@miami.edu>
2025-09-09 10:07:02 -0400

    added facet wrap

--- scripts/03_content/make_plot.R
+++ scripts/03_content/make_plot.R
@@ -10,9 +10,10 @@ data("data_heatwaves")
 p <- ggplot( data = data_heatwaves,
              mapping = aes(x = year, y = temp_mean,
                            group = paste(fishery, eu_rnpa))) +
-  geom_line(linewidth = 1.2) +
+  geom_line(linewidth = .8) +
+  facet_wrap(~ fishery, ncol = 2, scales = "free_y") +
   theme_bw()
 
 #export plot
-ggsave(plot = p,
-       filename = "results/img/mean_temp_per_year.png")
+ggsave(plot = p, filename = "results/img/mean_temp_per_year.png")
+p
